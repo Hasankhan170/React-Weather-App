011224a8b92595b118ea9a89678fb5e97c93bb1d
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-04 23:15:55 -0700

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // const [renderData,setRenderData] = useState([])
+  const inputVal = useRef()
+
+  try {
+    useEffect(()=>{
+      axios('http://api.weatherapi.com/v1/current.json?key=b90421cd7596432bbb2144327241406&q=&aqi=no')
+      .then((res)=>{
+        console.log(res.data);     
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    },[])
+  } catch (error) {
+    console.log(error);
+    
+  }
+  function searchWeather(){
+    console.log(inputVal.current.value);
+    
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello Weather</h1>
+      <input ref={inputVal} type="text" placeholder="Enter city name" />
+      <button onClick={searchWeather}>Search</button>
     </>
   )
 }
